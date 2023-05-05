b1d2c620449c5d43807a96711ab4fbcdac21f31c
Brent Lineberry <brent.lineberry@emory.edu>
2023-05-05 11:58:22 -0400

    needed to remove the socket connection code

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ const isDirectMention = require('./listenerMiddleware/isDirectMention');
 const app = new App({
     token: process.env.SLACK_BOT_TOKEN,
     signingSecret: process.env.SLACK_SIGNING_SECRET,
-    socketMode: true, // add this
-    appToken: process.env.SLACK_APP_TOKEN // add this
+    //socketMode: true, // add this
+    //appToken: process.env.SLACK_APP_TOKEN // add this
 });
 
 //app.message([loggerListener], async () => {
