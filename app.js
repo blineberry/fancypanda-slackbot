11db971cfa7ef445522ffc7e013435d88b2ebfc5
Brent Lineberry <brent.lineberry@emory.edu>
2023-05-08 15:12:31 -0400

    forgot to comment out the websocket lines

--- app.js
+++ app.js
@@ -13,8 +13,8 @@ const TeaCoffeeHandler = require('./handlers/teaCoffeeHandler');
 const app = new App({
     token: process.env.SLACK_BOT_TOKEN,
     signingSecret: process.env.SLACK_SIGNING_SECRET,
-    socketMode: true, // add this
-    appToken: process.env.SLACK_APP_TOKEN // add this
+    //socketMode: true, // add this
+    //appToken: process.env.SLACK_APP_TOKEN // add this
 });
 
 // Incoming logger
